e5305b21c512202db95072bd2f035c1ade90fab6
Art <artmg@users.noreply.github.com>
2017-01-08 19:48:37 +0000

    updates after testing and improved doc

--- Ps3ControlCamJamCar.py
+++ Ps3ControlCamJamCar.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
+"""Ps3ControlCamJamCar.py: Control CamJam Edukit 3 Robotics on Raspberry Pi with PS3 Controller over Bluetooth."""
+
+__author__ = "ArtMG"
+__contact__ = "https://github.com/artmg/MuGammaPi/blob/master/Ps3ControlCamJamCar.py"
+__status__ = "Production"
+
+# For more on this see the accompanying article https://github.com/artmg/MuGammaPi/wiki/CamJam-Robotics-Kit
 
 import RPi.GPIO as GPIO # GPIO output
 import os 				# shutdown
 import pygame 			# PS3 input
 
 
-# credit - http://www.instructables.com/id/Remote-Raspberry-Pi-Robot-PS3-Controller-Fablab-Ne/?ALLSTEPS
-
+# section credit - http://www.instructables.com/id/Remote-Raspberry-Pi-Robot-PS3-Controller-Fablab-Ne/?ALLSTEPS
 # Initialise the pygame library
 pygame.init()
 
@@ -16,35 +22,44 @@ j = pygame.joystick.Joystick(0)
 j.init()
 
 
-print 'Initialized Joystick : %s' % j.get_name()
-
-
-# credit https://github.com/CamJam-EduKit/EduKit3/blob/master/Code/4-driving.py
+# section credit https://github.com/CamJam-EduKit/EduKit3/blob/master/Code/4-driving.py
 
 # Set the GPIO modes
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+
+## SWAP THESE PAIRS DEPENDING on your wiring
+
 # Set variables for the GPIO motor pins
-pinMotorAForwards = 10
-pinMotorABackwards = 9
-pinMotorBForwards = 8
-pinMotorBBackwards = 7
+pinMotorRightForwards = 10
+pinMotorRightBackwards = 9
+
+pinMotorLeftForwards = 8
+pinMotorLeftBackwards = 7
+
 
 # Set the GPIO Pin mode
-GPIO.setup(pinMotorAForwards, GPIO.OUT)
-GPIO.setup(pinMotorABackwards, GPIO.OUT)
-GPIO.setup(pinMotorBForwards, GPIO.OUT)
-GPIO.setup(pinMotorBBackwards, GPIO.OUT)
+GPIO.setup(pinMotorLeftForwards, GPIO.OUT)
+GPIO.setup(pinMotorLeftBackwards, GPIO.OUT)
+GPIO.setup(pinMotorRightForwards, GPIO.OUT)
+GPIO.setup(pinMotorRightBackwards, GPIO.OUT)
 
 # Set all motors off
-GPIO.output(pinMotorAForwards, 0)
-GPIO.output(pinMotorABackwards, 0)
-GPIO.output(pinMotorBForwards, 0)
-GPIO.output(pinMotorBBackwards, 0)
+GPIO.output(pinMotorLeftForwards, 0)
+GPIO.output(pinMotorLeftBackwards, 0)
+GPIO.output(pinMotorRightForwards, 0)
+GPIO.output(pinMotorRightBackwards, 0)
 
 
-# credit - http://www.instructables.com/id/Remote-Raspberry-Pi-Robot-PS3-Controller-Fablab-Ne/?ALLSTEPS
+# credit - https://www.raspberrypi.org/forums/viewtopic.php?t=130201&p=869560
+os.putenv('DISPLAY', ':0.0') 
+pygame.display.init()
+# however this now forces you to **run as root** with sudo
+
+
+# section shared credit - https://www.raspberrypi.org/forums/viewtopic.php?f=32&t=163814
+# section shared credit - http://www.instructables.com/id/Remote-Raspberry-Pi-Robot-PS3-Controller-Fablab-Ne/?ALLSTEPS
 
 # Only start the motors when the inputs go above the following threshold
 threshold = 0.60
@@ -70,46 +85,51 @@ try:
             if event.axis == 1:
               LeftTrack = event.value
               UpdateMotors = 1
+
             elif event.axis == 3:
               RightTrack = event.value
               UpdateMotors = 1
 
+
             # Check if we need to update what the motors are doing
             if UpdateMotors:
 
-              # Check how to configure the left motor
-
-              # Move forwards
-              if (RightTrack > threshold):
-				  GPIO.output(pinMotorBForwards, 1)
-				  GPIO.output(pinMotorBBackwards, 0)
-              # Move backwards
-              elif (RightTrack < -threshold):
-				  GPIO.output(pinMotorBForwards, 0)
-				  GPIO.output(pinMotorBBackwards, 1)
-              # Stopping
-              else:
-				  GPIO.output(pinMotorBForwards, 0)
-				  GPIO.output(pinMotorBBackwards, 0)
-
-              # And do the same for the right motor
+              # Check how to configure the LEFT motor
+              # Move FORWARDS
               if (LeftTrack > threshold):
-				  GPIO.output(pinMotorAForwards, 1)
-				  GPIO.output(pinMotorABackwards, 0)
-              # Move backwards
+                  GPIO.output(pinMotorLeftForwards, 1)
+                  GPIO.output(pinMotorLeftBackwards, 0)
+              # Move BACKWARDS
               elif (LeftTrack < -threshold):
-				  GPIO.output(pinMotorAForwards, 0)
-				  GPIO.output(pinMotorABackwards, 1)
+                  GPIO.output(pinMotorLeftForwards, 0)
+                  GPIO.output(pinMotorLeftBackwards, 1)
               # Otherwise stop
               else:
-				  GPIO.output(pinMotorAForwards, 0)
-				  GPIO.output(pinMotorABackwards, 0)
+                  GPIO.output(pinMotorLeftForwards, 0)
+                  GPIO.output(pinMotorLeftBackwards, 0)
+
+              # And do the same for the RIGHT motor
+              # Move FORWARDS
+              if (RightTrack > threshold):
+                  GPIO.output(pinMotorRightForwards, 1)
+                  GPIO.output(pinMotorRightBackwards, 0)
+              # Move BACKWARDS
+              elif (RightTrack < -threshold):
+                  GPIO.output(pinMotorRightForwards, 0)
+                  GPIO.output(pinMotorRightBackwards, 1)
+              # Stopping
+              else:
+                  GPIO.output(pinMotorRightForwards, 0)
+                  GPIO.output(pinMotorRightBackwards, 0)
+
 
 
 except KeyboardInterrupt:
     # Turn off the motors
-	GPIO.output(pinMotorBForwards, 0)
-	GPIO.output(pinMotorBBackwards, 0)
+    GPIO.output(pinMotorLeftForwards, 0)
+    GPIO.output(pinMotorLeftBackwards, 0)
+    GPIO.output(pinMotorRightForwards, 0)
+    GPIO.output(pinMotorRightBackwards, 0)
     j.quit()#!/usr/bin/env python
 
 GPIO.cleanup()
